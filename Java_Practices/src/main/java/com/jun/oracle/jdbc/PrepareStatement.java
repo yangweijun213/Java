c0aab3f4e1926ba150c102ad5317bbbbe49fbdb8
Jeff Yang <yangweijun213@hotmail.com>
2016-09-30 17:58:09 +0800

    update

--- Java_Practices/src/main/java/com/jun/oracle/jdbc/PrepareStatement.java
+++ Java_Practices/src/main/java/com/jun/oracle/jdbc/PrepareStatement.java
@@ -11,9 +11,9 @@ import java.util.Date;
 import java.util.Properties;
 
 /**
- * 使用JDBC执行 select insert update delete
+ * 使用JDBC prepareStatement() 执行 select,insert,update,delete
  */
-public class SimpleOracleJDBC {
+public class PrepareStatement {
 
 	String dbDriver;
 	String dbHost;
@@ -23,35 +23,37 @@ public class SimpleOracleJDBC {
 	String dbConnPassword;
 
 	/**
-	 * Get oracle connection from db.properties
+	 * read file db.properities 读文件db.properities (key and value pair 键值对)
 	 */
 	public void getOracleConnection() {
 		try {
 
-			// create a object pro of Properties class
+			// create a object pro of Properties class 新建一个Properties类的对象
 			Properties pro = new Properties();
-			// create object inptStream of InputStream class, read db.properties
-			// into object inptStream
+			// create object inptStream of InputStream class and read
+			// db.properties
+			// 新建一个InputStream类的对象，读文件db.properties
 			InputStream inptStream = this.getClass().getClassLoader()
 					.getResourceAsStream("com/jun/oracle/jdbc/db.properties");
-			// load object inptStream of InputStream class
+			// load object inptStream of InputStream class 加载InputStream类的对象
 			pro.load(inptStream);
 
-			// get oracleDb_Driver value from db.properties
+			// get oracleDb_Driver value 读取oracleDb_Driver值
 			dbDriver = pro.getProperty("oracleDb_Driver");
-			// get oracleDb_Host value from db.properties
+			// get oracleDb_Host value 读取 oracleDb_Host值
 			dbHost = pro.getProperty("oracleDb_Host");
-			// get oracleDb_Port value from db.properties
+			// get oracleDb_Port value 读取 oracleDb_Port端口
 			dbPort = pro.getProperty("oracleDb_Port");
-			// get oracleDb_Instance value from db.properties
+			// get oracleDb_Instance value 读取oracle实例
 			dbInstance = pro.getProperty("oracleDb_Instance");
-			// get oracleDb_UserName value from db.properties
+			// get oracleDb_UserName value 读取oracle登录用户名
 			dbConnUsername = pro.getProperty("oracleDb_UserName");
-			// get oracleDb_Password value from db.properties
+			// get oracleDb_Password value 读取oracle登录密码
 			dbConnPassword = pro.getProperty("oracleDb_Password");
 
-			// Closes this input stream and releases any system resources
+			// Closes this inputstream and releases any system resources
 			// associated with the stream.
+			// 关闭输入流和释放关联输入流的资源
 			inptStream.close();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -60,28 +62,31 @@ public class SimpleOracleJDBC {
 	}
 
 	/**
-	 * Establishing a connection
+	 * Establishing a connection 建立连接
 	 */
 	public Connection establishConnection() {
 
-		// create a conn object of Connection class, this object is assigned
 		Connection conn = null;
 
-		// invoke dbGetProperity method
-		this.getOracleConnection();
 		try {
-			// define oracle connection url
-			String url = "jdbc:oracle:thin:@" + dbHost + ":" + dbPort + ":" + dbInstance + "";
-			// load database driver
+			// 注册驱动
+			this.getOracleConnection();
 			Class.forName(dbDriver);
-			// connect database
+
+			// 获取连接
+			String url = "jdbc:oracle:thin:@" + dbHost + ":" + dbPort + ":" + dbInstance + "";
 			conn = DriverManager.getConnection(url, dbConnUsername, dbConnPassword);
+
 		} catch (ClassNotFoundException e) {
+
 			// throws a ClassNotFound exception if driver load failure
+			// 如果驱动加载失败抛出异常
 			e.printStackTrace();
 			System.out.println("FAIL: Class not found when load oracle driver.\n");
 		} catch (SQLException e) {
+
 			// throws a database access exception if database connection failure
+			// 如果数据库连接失败抛出异常
 			e.printStackTrace();
 			System.out.println(
 					"FAIL: Error when getting database connection.\n" + e.getErrorCode() + " : " + e.getMessage());
@@ -89,35 +94,32 @@ public class SimpleOracleJDBC {
 		return conn;
 	}
 
-	
 	/**
-	 * 增加数据
+	 * insert data 添加数据
 	 */
 	public void insertData(int userID, String userName, String createdBy, Date startTime) throws Exception {
-
-		// declare pstmt object of PreparedStatement class
-		PreparedStatement pstmt = null;
-		// declare rs object of ResultSet class
-		ResultSet rs = null;
-		// declare conn object of Connection class
+		
 		Connection conn = null;
-
-		// instantiate object myJdbcUtils of JdbcUtils class
-		SimpleOracleJDBC myJdbcUtils = new SimpleOracleJDBC();
-		// invoke dbConnection method of myJdbcUtils object
-		conn = myJdbcUtils.establishConnection();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		// 连接数据库
+		PrepareStatement myPreStmt = new PrepareStatement();
+		conn = myPreStmt.establishConnection();
 
 		// get the current state of this Connection object's auto-commit mode
+		// 获得自动提交的状态
 		boolean defaultCommit = conn.getAutoCommit();
+
 		/* 开启事务，设定不自动提交 */
 		conn.setAutoCommit(false);
 
-		// 插入
 		try {
-			String sq2 = "insert into DBUSER (USER_ID,USERNAME,CREATED_BY,CREATED_DATE) values (?,?,?,?)";
-			// 创建该连接下的PreparedStatement对象
-			pstmt = conn.prepareStatement(sq2);
+			// 定义sql语句，values用？代替
+			String sq1 = "insert into DBUSER (USER_ID,USERNAME,CREATED_BY,CREATED_DATE) values (?,?,?,?)";
 
+			// 创建该连接下的PreparedStatement对象
+			pstmt = conn.prepareStatement(sq1);
 			// 传递第一个参数值 ，代替第一个问号
 			pstmt.setInt(1, userID);
 			// 传递第二个参数值 ，代替第二个问号
@@ -127,7 +129,7 @@ public class SimpleOracleJDBC {
 			// 传递第四个参数值 ，代替第四个问号
 			pstmt.setTimestamp(4, new Timestamp(System.currentTimeMillis()));
 
-			// 执行查询语句，将数据保存到ResultSet对象中
+			// 执行SQL，将数据保存到ResultSet对象中
 			rs = pstmt.executeQuery();
 
 			/* 正式提交 */
@@ -143,28 +145,24 @@ public class SimpleOracleJDBC {
 		finally {
 			/* 恢复原提交状态 */
 			conn.setAutoCommit(defaultCommit);
-			// close the connection
-			myJdbcUtils.colseConnection(conn, pstmt, rs);
+			// 释放资源
+			myPreStmt.colseConnection(conn, pstmt, rs);
 		}
 
 	}
-	
+
 	/**
-	 *  查询数据           
+	 * select data 查询数据
 	 */
 	public void selectData() throws Exception {
-
-		// declare pstmt object of PreparedStatement class
-		PreparedStatement pstmt = null;
-		// declare rs object of ResultSet class
-		ResultSet rs = null;
-		// declare conn object of Connection class
+		
 		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 
-		// instantiate object myJdbcUtils of JdbcUtils class
-		SimpleOracleJDBC myJdbcUtils = new SimpleOracleJDBC();
-		// invoke dbConnection method of myJdbcUtils object
-		conn = myJdbcUtils.establishConnection();
+		// 连接数据库
+		PrepareStatement myPreStmt = new PrepareStatement();
+		conn = myPreStmt.establishConnection();
 
 		// get the current state of this Connection object's auto-commit mode
 		boolean defaultCommit = conn.getAutoCommit();
@@ -172,18 +170,20 @@ public class SimpleOracleJDBC {
 		conn.setAutoCommit(false);
 
 		try {
-			String sql1 = "select * from DBUSER";
-			// 创建该连接下的PreparedStatement对象
-			pstmt = conn.prepareStatement(sql1);
+			String sql2 = "select * from DBUSER";
 
-			// 执行查询语句，将数据保存到ResultSet对象中
+			// 创建该连接下的PreparedStatement对象
+			pstmt = conn.prepareStatement(sql2);
+
+			// 执行SQL，将数据保存到ResultSet对象中
 			rs = pstmt.executeQuery();
 
+			// 每次循环，都取出一条记录
 			while (rs.next()) {
 				System.out.println(rs.getInt("USER_ID") + rs.getString("USERNAME") + rs.getString("CREATED_BY")
 						+ rs.getTimestamp("CREATED_DATE"));
 			}
-			
+
 			/* 正式提交 */
 			conn.commit();
 			System.out.println("查询成功");
@@ -192,32 +192,28 @@ public class SimpleOracleJDBC {
 			conn.rollback();
 			throw ex;
 		}
-				
+
 		finally {
 			/* 恢复原提交状态 */
 			conn.setAutoCommit(defaultCommit);
-			// close the connection
-			myJdbcUtils.colseConnection(conn, pstmt, rs);
+			// 释放资源
+			myPreStmt.colseConnection(conn, pstmt, rs);
 		}
 
 	}
-	
+
 	/**
-	 *  更新数据       
+	 * update data 更新数据
 	 */
 	public void updateData() throws Exception {
 
-		// declare pstmt object of PreparedStatement class
 		PreparedStatement pstmt = null;
-		// declare rs object of ResultSet class
 		ResultSet rs = null;
-		// declare conn object of Connection class
 		Connection conn = null;
 
-		// instantiate object myJdbcUtils of JdbcUtils class
-		SimpleOracleJDBC myJdbcUtils = new SimpleOracleJDBC();
-		// invoke dbConnection method of myJdbcUtils object
-		conn = myJdbcUtils.establishConnection();
+		// 连接数据库
+		PrepareStatement myPreStmt = new PrepareStatement();
+		conn = myPreStmt.establishConnection();
 
 		// get the current state of this Connection object's auto-commit mode
 		boolean defaultCommit = conn.getAutoCommit();
@@ -225,48 +221,44 @@ public class SimpleOracleJDBC {
 		conn.setAutoCommit(false);
 
 		try {
-			String sql3 = "update DBUSER set username='Jack' where created_by='jeff'";
+			String sql3 = "update DBUSER set username='Jeff' where created_by='PrepareStatement'";
 			// 创建该连接下的PreparedStatement对象
 			pstmt = conn.prepareStatement(sql3);
 
 			// 执行查询语句，将数据保存到ResultSet对象中
 			rs = pstmt.executeQuery();
-			
+
 			/* 正式提交 */
 			conn.commit();
 			System.out.println("更新成功");
-			
+
 		} catch (Exception ex) {
 			/* 出错回滚 */
 			conn.rollback();
 			throw ex;
 		}
-				
+
 		finally {
 			/* 恢复原提交状态 */
 			conn.setAutoCommit(defaultCommit);
-			// close the connection
-			myJdbcUtils.colseConnection(conn, pstmt, rs);
+			// 释放资源
+			myPreStmt.colseConnection(conn, pstmt, rs);
 		}
 
 	}
-	
+
 	/**
-	 *  删除数据       
+	 * delete data 删除数据
 	 */
 	public void deleteData() throws Exception {
-
-		// declare pstmt object of PreparedStatement class
-		PreparedStatement pstmt = null;
-		// declare rs object of ResultSet class
-		ResultSet rs = null;
-		// declare conn object of Connection class
+		
 		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 
-		// instantiate object myJdbcUtils of JdbcUtils class
-		SimpleOracleJDBC myJdbcUtils = new SimpleOracleJDBC();
-		// invoke dbConnection method of myJdbcUtils object
-		conn = myJdbcUtils.establishConnection();
+		// 连接数据库
+		PrepareStatement myPreStmt = new PrepareStatement();
+		conn = myPreStmt.establishConnection();
 
 		// get the current state of this Connection object's auto-commit mode
 		boolean defaultCommit = conn.getAutoCommit();
@@ -274,34 +266,34 @@ public class SimpleOracleJDBC {
 		conn.setAutoCommit(false);
 
 		try {
-			String sql4 = "delete from DBUSER where created_by='jeff'";
+			String sql4 = "delete from DBUSER where created_by='PrepareStatement'";
 			// 创建该连接下的PreparedStatement对象
 			pstmt = conn.prepareStatement(sql4);
 
-			// 执行查询语句，将数据保存到ResultSet对象中
+			// 执行SQL，将数据保存到ResultSet对象中
 			rs = pstmt.executeQuery();
-			
+
 			/* 正式提交 */
 			conn.commit();
 			System.out.println("删除成功");
-			
+
 		} catch (Exception ex) {
 			/* 出错回滚 */
 			conn.rollback();
 			throw ex;
 		}
-				
+
 		finally {
 			/* 恢复原提交状态 */
 			conn.setAutoCommit(defaultCommit);
-			// close the connection
-			myJdbcUtils.colseConnection(conn, pstmt, rs);
+			// 释放资源
+			myPreStmt.colseConnection(conn, pstmt, rs);
 		}
 
 	}
 
 	/**
-	 * 关闭连接 close the connection
+	 * close the connection 释放资源
 	 */
 	public void colseConnection(Connection conn, PreparedStatement pstmt, ResultSet rs) {
 
--- Java_Practices/src/main/java/com/jun/oracle/jdbc/RunMain.java
+++ Java_Practices/src/main/java/com/jun/oracle/jdbc/RunMain.java
@@ -5,16 +5,20 @@ import java.util.Date;
 public class RunMain {
 
 	public static void main(String[] args) throws Exception {
-
-		SimpleOracleJDBC myJdbcUtils = new SimpleOracleJDBC();
-		//插入数据
-		myJdbcUtils.insertData(1, "jeff", "jeff", new Date());
-		//查询数据
-		myJdbcUtils.selectData();
-		//更新数据
-		myJdbcUtils.updateData();
-		//删除数据
-		myJdbcUtils.deleteData();
+		
+		//使用JDBC prepareStamtement执行 insert,select,update,delete
+//		PrepareStatement mypstmt = new PrepareStatement();
+//		mypstmt.insertData(1, "PrepareStatement", "PrepareStatement", new Date());
+//		mypstmt.selectData();
+//		mypstmt.updateData();
+//		mypstmt.deleteData();
+		
+		// 使用JDBC createStamtement执行 insert,select,update,delete
+		CreateStatement mystmt = new CreateStatement();
+		mystmt.insertData();
+		mystmt.selectData();
+//		mystmt.updateData();
+//		mystmt.deleteData();
 
 	}
 
