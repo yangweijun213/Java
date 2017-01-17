4f8f491d78d6d14064c89fa8a0fd70dd3a9b2f0d
Jeff Yang <Jeff.Yang@lombardrisk.com>
2017-01-17 21:07:16 +0800

    update

---
+++ JavaStart/src/com/jun/datatype/TestDataType.java
@@ -0,0 +1,21 @@
+package com.jun.datatype;
+
+public class TestDataType {
+
+	public static void main (String[] args){
+		
+		byte a =1;//占用存储1个字节
+		short b = 2; //占用存储2个字节
+		int c =3; //占用存储4个字节
+		long d = 44444444444L;//占用存储8个字节, 末尾加L
+		
+		float e =3.14F; //占用存储4个字节，末尾加F
+		double f = 3.14512;//占用存储8个字节
+		
+		char ggg = 'a';//字符，占用存储2个字节，需要用引号
+		String h ="abcsdd"; //字符串，占用存储双引号，序列花字符
+	
+		
+	}
+
+}
