a366664d54a400235855df5d8118ab55c062f10f
Jeff Yang <Jeff.Yang@lombardrisk.com>
2017-01-23 19:03:07 +0800

    update

---
+++ JavaStart/src/com/jun/stream/TestCopyFolderAndFile.java
@@ -0,0 +1,72 @@
+package com.jun.stream;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class TestCopyFolderAndFile {
+
+	static void copyDir(File file, File file2) {
+
+		// 如果目标目录没有，就新建一个目录
+		new File(file2, file.getName()).mkdir();
+		File[] files = file.listFiles();
+		for (File f : files) {
+			if (f.isDirectory()) {
+				copyDir(f, new File(file2, file.getName()));
+			}
+			if (f.isFile()) {
+				copyFile (f,new File(file2, file.getName()));
+			}
+		}
+	}
+
+	static void copyFile(File file, File file2) {
+		FileInputStream fis = null;
+		FileOutputStream fos = null;
+		byte[] buffer = new byte[1024];
+		int temp = 0;
+
+		try {
+			
+			fis = new FileInputStream(file);
+			fos = new FileOutputStream(file2);
+			while ((temp = fis.read(buffer)) != -1) {
+				// - write(byte[] b, int off, int len)
+				fos.write(buffer, 0, temp);
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fos.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				fis.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		// 复制目录, 将stream目录下面的所有目录，复制到D:\test目录下
+		File sourceFile = new File("D:\\workspace\\Java\\JavaStart\\src\\com\\jun\\stream\\a\\1.txt");
+		File tagetFile = new File("D:\\test\\1.txt");
+		//TestCopyFolderAndFile.copyDir(sourceFile, tagetFile);
+		TestCopyFolderAndFile.copyFile(sourceFile, tagetFile);
+	}
+
+}
--- JavaStart/src/com/jun/stream/TestFileInputStreams.java
+++ JavaStart/src/com/jun/stream/TestFileInputStreams.java
@@ -6,9 +6,9 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class TestFileInputStream  {
+public class TestFileInputStreams  {
 	
-	//读文件
+	//读文件 - read()方法，一个个字节读
 	static void testRead() {	
 		File f = new File ("F:\\yangweijun\\workspace\\Java\\JavaStart\\src\\com\\jun\\stream\\a.txt");
 		FileInputStream fis = null;
@@ -16,7 +16,9 @@ public class TestFileInputStream  {
 		try {
 			fis = new FileInputStream (f);
 			int i ;
+			//从此输入流中读取一个数据字节
 			while ((i=fis.read())!= -1) {
+				//强转：int转换char
 				char a = (char)i;
 				System.out.print(a);
 			}
@@ -28,6 +30,7 @@ public class TestFileInputStream  {
 			e.printStackTrace();
 		}finally {
 			try {
+				//用来之后要关闭
 				if (fis!=null){
 					fis.close();
 				}
@@ -39,45 +42,13 @@ public class TestFileInputStream  {
 	}
 	
 	
-	//写文件
-	static void testWrite() {
-			FileOutputStream fos = null;
-			String str = "abcdefdfddfdfdf";
-			try {
-				fos = new FileOutputStream ("F:\\yangweijun\\workspace\\Java\\JavaStart\\src\\com\\jun\\stream\\b.txt");
-				//fos.write('c');
-				
-//				for (int i=0; i<str.length();i++){
-//					fos.write(str.charAt(i));
-//				}
-				
-				//转成字符
-				fos.write(str.getBytes());
-				
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} finally {
-				try {
-					if (fos!=null){
-						fos.close();
-					}
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		
-	}
+	
 	
 	public static void main(String[] args) {
 		
 		//读文件
-		//testRead();
-		testWrite();
+		testRead();
+	
 		
 	}
 }
