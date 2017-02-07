3bd64e0c936c90b760d4b5ba27abecf84699a782
Jeff Yang <Jeff.Yang@lombardrisk.com>
2017-02-07 18:45:05 +0800

    update

--- SpringMVC/02springmvc_hello/src/cn/sxt/controller/HelloController.java
+++ SpringMVC/02springmvc_hello/src/cn/sxt/controller/HelloController.java
@@ -17,10 +17,10 @@ public class HelloController implements Controller{
 		System.out.println("-----hello controller-------"); 
 		//创建一个ModelAndView对象  该对象最后被视图渲染器渲染为指定视图
 		ModelAndView mv = new ModelAndView();
-		//设置视图名称 ----最后解析为指定的页面
-		mv.setViewName("hello");
-		//添加结果数据  该数据可以在页面中通过el表达式获取
+		//封装要显示到视图中的数据 （添加结果数据  该数据可以在页面中通过el表达式获取）
 		mv.addObject("msg", "first spring mvc app");
+		//设置视图名称 ----最后解析为指定的页面
+		mv.setViewName("hello");  //相当于去web-inf/jsp/hello.jsp
 		return mv;
 	}
 }
