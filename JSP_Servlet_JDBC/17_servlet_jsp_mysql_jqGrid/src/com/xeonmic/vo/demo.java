84481ffb1e8bc3eaf80aff27d74460a158c610ce
yangweijun213 <yangweijun213@hotmail.com>
2017-03-05 14:47:29 +0800

    update

--- JSP_Servlet_JDBC/17_servlet_jsp_mysql_jqGrid/src/com/xeonmic/vo/demo.java
+++ JSP_Servlet_JDBC/17_servlet_jsp_mysql_jqGrid/src/com/xeonmic/vo/demo.java
@@ -50,7 +50,7 @@ public class demo {
 		this.text = text;
 	}
 
-	//²âÊÔ²âÊÔ²âÊÔ°¡
+	//²âÊÔ²âÊÔ²âÊÔ°¡µÄµÄ
 	@Override
 	public String toString() {
 		return "demo [id=" + id + ", name=" + name + ", type=" + type
