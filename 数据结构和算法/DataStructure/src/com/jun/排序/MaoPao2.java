133d7e6f632976aa300c30b951e895a4fb6dc493
yangweijun213 <yangweijun213@hotmail.com>
2017-05-10 21:32:46 +0800

    update

---
+++ 数据结构和算法/DataStructure/src/com/jun/排序/MaoPao2.java
@@ -0,0 +1,31 @@
+package com.jun.排序;
+
+public class MaoPao2 {
+
+	public static void main(String[] args) {
+		int[] list = { 7, 6, 5, 4, 3, 2, 1 };
+		int[] list2 = { 1, 2, 3, 4, 5, 6, 7 };
+		funBubbleSort(list2);
+	}
+
+	// 冒泡排序 - 从大到小排序
+	static void funBubbleSort(int[] array){
+		boolean flag = true;
+		for (int i=0;i<array.length-1&&flag;i++){
+			flag = false;
+			for (int j=0;j<array.length-1-i;j++){
+				if (array[j]<array[j+1]){
+					int temp = array[j+1];
+					array[j+1]=array[j];
+					array[j]=temp;
+					flag=true;
+				}
+			}
+		}
+		
+		for (int i=0; i<array.length;i++){
+			System.out.println(array[i]);
+		}
+	}
+
+}
